4b7bb6c38c00cd0607eb770add24908aa5b1980f
Marvin <marvnluck@gmail.com>
2022-05-18 20:25:56 +0700

    Add Timer Script

---
+++ Assets/Blockit/Scripts/Game/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    bool timerActive = false;
+    float currentTime;
+    public Text currentTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerActive == true)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:fff");
+    }
+
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+
+    public void StopTimer()
+    {
+        timerActive = false;
+    }
+}
